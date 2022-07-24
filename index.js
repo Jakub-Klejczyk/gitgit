4aacefc69ad772d5e89abb7a04566fd056a1dc81
Jakub <jakub-klejczyk@o2.pl>
2022-07-24 21:00:54 +0200

    test

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const email = document.querySelector("#email");
 const submit = document.querySelector("#submit");
+const form = document.querySelector("#form");
+const username = document.querySelector("#name");
+const password = document.querySelector("#password");
 
 const pattern =
   /^("(?:[!#-\[\]-\u{10FFFF}]|\\[\t -\u{10FFFF}])*"|[!#-'*+\-/-9=?A-Z\^-\u{10FFFF}](?:\.?[!#-'*+\-/-9=?A-Z\^-\u{10FFFF}])*)@([!#-'*+\-/-9=?A-Z\^-\u{10FFFF}](?:\.?[!#-'*+\-/-9=?A-Z\^-\u{10FFFF}])*|\[[!-Z\^-\u{10FFFF}]*\])$/u;
 
-submit.addEventListener("click", (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   if (email.value.match(pattern)) {
-    console.log("OK");
+    console.log("Jest OK");
   } else {
     console.log("Kurwa nie OK");
   }
